c341ce549ee18aba2b63b0d5d00b1853f3e55e74
acisliver <7273322@naver.com>
2022-02-19 10:40:11 +0900

    @Configuration으로 Bean 관리의 장점

--- src/main/java/com/example/springbasic/SpringConfig.java
+++ src/main/java/com/example/springbasic/SpringConfig.java
@@ -6,6 +6,8 @@ import com.example.springbasic.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// 상황에 따라 구현 클래스를 변경해야할 때
+// 여기서 구현 클래스만 바꿔주면 된다 - 장점
 @Configuration
 public class SpringConfig {
 
