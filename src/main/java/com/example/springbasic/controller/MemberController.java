b9ef894b42de4edb40782060c06432c2b0bcec2c
acisliver <7273322@naver.com>
2022-02-22 07:13:19 +0900

    회원 웹 기능 - 조회

--- src/main/java/com/example/springbasic/controller/MemberController.java
+++ src/main/java/com/example/springbasic/controller/MemberController.java
@@ -5,8 +5,11 @@ import com.example.springbasic.domain.Member;
 import com.example.springbasic.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링 컨테이너에 컨트롤러 등록 (컴포넌트 스캔)
 public class MemberController {
@@ -38,5 +41,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
