ea01149f382cdfa69f0a0a5c6067a2a920600a64
acisliver <7273322@naver.com>
2022-05-03 23:24:42 +0900

    순수 Jdbc - OCP(개방-폐쇄원칙)

--- src/main/java/com/example/springbasic/SpringConfig.java
+++ src/main/java/com/example/springbasic/SpringConfig.java
@@ -1,23 +1,36 @@
 package com.example.springbasic;
 
+import com.example.springbasic.repository.JdbcMemberRepository;
 import com.example.springbasic.repository.MemberRepository;
 import com.example.springbasic.repository.MemoryMemberRepository;
 import com.example.springbasic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 상황에 따라 구현 클래스를 변경해야할 때
 // 여기서 구현 클래스만 바꿔주면 된다 - 장점
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    // config에서 구현체만 바꿔도 실행이 잘 된다.
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/com/example/springbasic/repository/MemberRepository.java
+++ src/main/java/com/example/springbasic/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+// 이 인터페이스를 구현하는 방법에 따라 DB 접근 방법이 달라진다
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
